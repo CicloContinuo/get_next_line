6a9171a4198700eb9eca426b0a0a11c63ca66ec1
ciclocontinuo <ciclocontinuo2@gmail.com>
2022-06-16 22:18:26 +0200

    0

---
+++ src/examples/1/src/include/get_next_line.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ciclo <ciclo@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/16 21:49:44 by ciclo             #+#    #+#             */
+/*   Updated: 2022/06/16 22:14:03 by ciclo            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE
+# define GET_NEXT_LINE
+
+# include	<stdio.h>
+# include <stdio.h>
+# include	<unistd.h>
+# include <fcntl.h>
+
+
+//char	*get_next_line(int fd);
+#endif
