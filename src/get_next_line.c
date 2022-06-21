c147ed4a22ff96459cfb714811286b22490332cb
DuvanGonzalez-ciclo <ciclocontinuo2@gmail.com>
2022-06-21 16:03:51 +0200

    0.01%

--- src/get_next_line.c
+++ src/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: ciclo-d <ciclo-d@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/21 11:04:43 by ciclo-d           #+#    #+#             */
-/*   Updated: 2022/06/21 15:42:23 by ciclo-d          ###   ########.fr       */
+/*   Updated: 2022/06/21 16:03:34 by ciclo-d          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,15 +18,17 @@ char	*get_next_line(int fd)
 {
 	//static char	*full_str;
 	char 		*buffer;
-	int			ft_read;
+	int			rd;
 
 	if (fd <= 0 || BUFFER_SIZE < 1)
 		return (NULL);
 	buffer = (char *)malloc(BUFFER_SIZE + 1);
 		if (!buffer)
 			return (NULL);
-	ft_read = read(fd, buffer, BUFFER_SIZE);
-	printf ("%d ", ft_read);
+	rd = 1;
+	//while (rd > 0)
+		rd = read(fd, buffer, BUFFER_SIZE);
+	printf ("%d ", rd);
 	return (buffer);
 }
 
