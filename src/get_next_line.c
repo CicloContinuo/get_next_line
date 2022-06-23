81bb489cbdde9f907b66d52f717b503b6c1f1141
DuvanGonzalez-ciclo <ciclocontinuo2@gmail.com>
2022-06-23 11:28:57 +0200

    read and join string

--- src/get_next_line.c
+++ src/get_next_line.c
@@ -1,3 +1,4 @@
+
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
@@ -6,35 +7,80 @@
 /*   By: ciclo-d <ciclo-d@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/21 11:04:43 by ciclo-d           #+#    #+#             */
-/*   Updated: 2022/06/21 16:03:34 by ciclo-d          ###   ########.fr       */
+/*   Updated: 2022/06/22 13:40:13 by ciclo-d          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "include/get_next_line.h"
 
-#include "include/get_next_line.h"
+char	*ft_read(int fd, char *str)
+{
+	char	*temp;
+	int		rd;
+
+	temp = (char *)malloc(BUFFER_SIZE + 1);
+	if (!temp)
+		return (NULL);
+	rd = 1;
+	while (!ft_strchr(temp, '\n') && rd != 0)
+	{
+		rd = read(fd, temp, BUFFER_SIZE);
+		if (rd == -1)
+		{
+			temp = malloc(sizeof(1));
+			temp[rd] =  '\0';
+			free(temp);
+		}
+		temp[rd] = '\0';
+		str = ft_strjoin(str, temp);
+	}
+	free(temp);
+	return (str);
+}
+
+
+
+/*char	*ft_read_and_save(int fd, char *save)
+{
+	char	*buff;
+	int		read_bytes;
+
+	buff = malloc((BUFFER_SIZE + 1) * sizeof(char));
+	if (!buff)
+		return (NULL);
+	read_bytes = 1;
+	while (!ft_strchr(save, '\n') && read_bytes != 0)
+	{
+		read_bytes = read(fd, buff, BUFFER_SIZE);
+		if (read_bytes == -1)
+		{
+			free(buff);
+			return (NULL);
+		}
+		buff[read_bytes] = '\0';
+		save = ft_strjoin(save, buff);
+	}
+		//printf ("%s", save);
+	free(buff);
+	return (save);
+}*/
 
 char	*get_next_line(int fd)
 {
-	//static char	*full_str;
-	char 		*buffer;
-	int			rd;
+	static char	*str;
+	char		*line;
 
 	if (fd <= 0 || BUFFER_SIZE < 1)
 		return (NULL);
-	buffer = (char *)malloc(BUFFER_SIZE + 1);
-		if (!buffer)
-			return (NULL);
-	rd = 1;
-	//while (rd > 0)
-		rd = read(fd, buffer, BUFFER_SIZE);
-	printf ("%d ", rd);
-	return (buffer);
+
+	//str = ft_read (fd);
+	//while (str != 0)
+	line = ft_read(fd, str);
+	return (line);
 }
 
 int	main(void)
 {
-
 	int		fd;
 
 	fd =  open("txt/fd.txt", O_RDONLY);
--- src/include/get_next_line.h
+++ src/include/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: ciclo-d <ciclo-d@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/21 11:02:15 by ciclo-d           #+#    #+#             */
-/*   Updated: 2022/06/21 13:24:47 by ciclo-d          ###   ########.fr       */
+/*   Updated: 2022/06/22 13:52:01 by ciclo-d          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,9 @@
 # include <stdlib.h>
 
 char	*get_next_line(int fd);
-char	*ft_strjoin(char *buffer, char *full_str);
-size_t	ft_strlen(const char *s);
+char	*ft_strjoin(char *s1, char *s2);
+size_t	ft_strlen(char *s);
+char	*ft_strchr(char *str, int c);
+void	*ft_memmove(void *dest, const void *src, size_t n);
 
 #endif
