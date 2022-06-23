74d373611b4e877f64292beec2962c39d6bfa3bc
DuvanGonzalez-ciclo <ciclocontinuo2@gmail.com>
2022-06-23 11:38:09 +0200

    read and join

--- src/get_next_line.c
+++ src/get_next_line.c
@@ -39,32 +39,6 @@ char	*ft_read(int fd, char *str)
 }
 
 
-
-/*char	*ft_read_and_save(int fd, char *save)
-{
-	char	*buff;
-	int		read_bytes;
-
-	buff = malloc((BUFFER_SIZE + 1) * sizeof(char));
-	if (!buff)
-		return (NULL);
-	read_bytes = 1;
-	while (!ft_strchr(save, '\n') && read_bytes != 0)
-	{
-		read_bytes = read(fd, buff, BUFFER_SIZE);
-		if (read_bytes == -1)
-		{
-			free(buff);
-			return (NULL);
-		}
-		buff[read_bytes] = '\0';
-		save = ft_strjoin(save, buff);
-	}
-		//printf ("%s", save);
-	free(buff);
-	return (save);
-}*/
-
 char	*get_next_line(int fd)
 {
 	static char	*str;
