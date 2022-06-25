ba265956b68249bae6038336ca7d2fd730c01fa5
Duvan Gonzalez <103336552+duvangonzalez-ciclo@users.noreply.github.com>
2022-06-25 14:48:44 +0200

    Ds

--- src/get_next_line.c
+++ src/get_next_line.c
@@ -6,20 +6,36 @@
 /*   By: ciclo <ciclo@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/21 11:04:43 by ciclo-d           #+#    #+#             */
-/*   Updated: 2022/06/25 14:18:18 by ciclo            ###   ########.fr       */
+/*   Updated: 2022/06/25 14:48:25 by ciclo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "include/get_next_line.h"
 
-int	main(void)
+char	*get_next_line(int fd)
 {
 	char	*buffer;
-	int		fd;
 	int		rd;
 
-	fd = open("txt/fd", O_RDONLY);
-	rd = read(fd, buffer, 5);
-	printf("%d\n", rd);
-	printf("%s\n", buffer);
+	if (fd < 0 || BUFFER_SIZE < 0)
+		return (NULL);
+	buffer = (char *)malloc(BUFFER_SIZE + 1);
+	if (!buffer)
+		return (NULL);
+	while (rd--)
+	{
+		rd = read(fd, buffer, BUFFER_SIZE);
+	}
+	//printf ("%s0", buffer);
+	return (buffer);
+}
+
+int	main(void)
+{
+	const char	*s;
+	int			fd;
+
+	fd = open("txt/fd.txt", O_RDONLY);
+	s = get_next_line(fd);
+	printf("%s\n", s);
 }
