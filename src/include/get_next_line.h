503f6fe3a39de16145a2896290287b866522980d
Duvan Gonzalez <103336552+duvangonzalez-ciclo@users.noreply.github.com>
2022-06-25 13:55:33 +0200

    s

--- src/include/get_next_line.h
+++ src/include/get_next_line.h
@@ -3,24 +3,20 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ciclo-d <ciclo-d@student.42.fr>            +#+  +:+       +#+        */
+/*   By: ciclo <ciclo@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/06/21 11:02:15 by ciclo-d           #+#    #+#             */
-/*   Updated: 2022/06/22 13:52:01 by ciclo-d          ###   ########.fr       */
+/*   Created: 2022/06/25 13:44:41 by ciclo             #+#    #+#             */
+/*   Updated: 2022/06/25 13:55:04 by ciclo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+
+# include <string.h>
 # include <stdio.h>
-# include <unistd.h>
-# include <fcntl.h>
 # include <stdlib.h>
 
 char	*get_next_line(int fd);
-char	*ft_strjoin(char *s1, char *s2);
-size_t	ft_strlen(char *s);
-char	*ft_strchr(char *str, int c);
-void	*ft_memmove(void *dest, const void *src, size_t n);
 
 #endif
