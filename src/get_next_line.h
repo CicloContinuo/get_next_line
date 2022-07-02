8df597e46666cb9b06081100d90b09a911d7fe30
Duvan Gonzalez <103336552+duvangonzalez-ciclo@users.noreply.github.com>
2022-07-02 12:07:53 +0200

    3.1%

--- src/get_next_line.h
+++ src/get_next_line.h
@@ -6,13 +6,13 @@
 /*   By: ciclo <ciclo@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/21 11:02:15 by ciclo-d           #+#    #+#             */
-/*   Updated: 2022/07/01 22:33:08 by ciclo            ###   ########.fr       */
+/*   Updated: 2022/07/02 12:01:16 by ciclo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# define BUFFER_SIZE 4
+//# define BUFFER_SIZE 4
 
 # include <stdio.h>
 # include <unistd.h>
@@ -25,9 +25,5 @@ char	*get_next_line(int fd);
 char	*ft_strjoin(char *s1, char *s2);
 size_t	ft_strlen(char *s);
 char	*ft_strchr(char *str, int c);
-void	*ft_memmove(void *dest, const void *src, size_t n);
-int		ft_slen(char *string, char c);
-char	*ft_linejoin(char *full);
-char	*ft_substr(char *full, int start, int len);
 
 #endif
--- src/get_next_line_utils.c
+++ src/get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: ciclo <ciclo@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/21 11:31:05 by ciclo-d           #+#    #+#             */
-/*   Updated: 2022/07/01 21:18:03 by ciclo            ###   ########.fr       */
+/*   Updated: 2022/07/01 22:56:43 by ciclo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,15 +40,15 @@ char	*ft_strchr(char *s, int c)
 {
 	int	i;
 
-	i = 0;
 	if (!s)
 		return (0);
 	if (c == '\0')
-		return ((char *)&s[ft_strlen(s)]);
+		return (&s[ft_strlen(s)]);
+	i = 0;
 	while (s[i] != '\0')
 	{
 		if (s[i] == (char) c)
-			return ((char *)&s[i]);
+			return (&s[i]);
 		i++;
 	}
 	return (0);
@@ -56,16 +56,16 @@ char	*ft_strchr(char *s, int c)
 
 char	*ft_strjoin(char *s1, char *s2)
 {
+	char	*str;
 	size_t	i;
 	size_t	c;
-	char	*str;
 
 	if (!s1)
 	{
 		s1 = (char *)malloc(1 * sizeof(char));
 		s1[0] = '\0';
 	}
-	if (!s1 || !s2)
+	if (!s1 && !s2)
 		return (NULL);
 	str = malloc((ft_strlen(s1) + ft_strlen(s2) + 1) * sizeof(char));
 	if (str == NULL)
@@ -89,58 +89,11 @@ int	ft_slen(char *string, char c)
 	i = 0;
 	if (!string)
 		return (0);
-	while (string[i])
+	while (string[i] != 0)
 	{
 		if (string[i] == c)
 			return (i);
 		i++;
 	}
 	return (i);
-}
-
-char	*ft_linejoin(char *full)
-{
-	char	*s;
-	int		i;
-	int		j;
-
-	j = ft_slen(full, '\n');
-	if (!full)
-		return (NULL);
-	s = (char *)malloc(j + 2);
-	if (!s)
-		return (NULL);
-	i = 0;
-	while (i < j + 1)
-	{
-		s[i] = full[i];
-		i++;
-	}
-	s[i] = '\0';
-	return (s);
-}
-
-char	*ft_substr(char *full, int start, int len)
-{
-	char	*s;
-	int		i;
-	int		j;
-
-	if (!ft_strchr(full, '\n'))
-	{
-		free(full);
-		return (NULL);
-	}
-	s = (char *)malloc((len - start + 1) * sizeof(char));
-	if (!s)
-		return (NULL);
-	i = 0;
-	j = 0;
-	while (i < start)
-		i++;
-	while (i < len)
-		s[j++] = full[i++];
-	s[j] = '\0';
-	free (full);
-	return (s);
 }
